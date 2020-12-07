e88cb566410404a65dd8c266e7c9c83eec3bb875
AvishekPD <57665042+AvishekPD@users.noreply.github.com>
2020-12-07 12:38:01 +0530

    dec gapps size

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const int gappx		    = 10;	/* gaps between teh windows */
+static const int gappx		    = 5;	/* gaps between teh windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
